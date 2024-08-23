0856edb4849f3c7e78c742cf9df7d19e9c001ed7
miyokonakada <fortissimo_pikkoro@yahoo.co.jp>
2024-08-23 15:31:37 +1000

    updated icon 0823

--- src/resources/views/layout/app.blade.php
+++ src/resources/views/layout/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>CoachTechフリマ</title>
+    <link rel="icon" href=" {{ asset('img/icon.png') }}">
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     <link rel="stylesheet" href="{{ asset('css/menu.css') }}">
